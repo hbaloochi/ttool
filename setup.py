ab5d6ae4cea2c77da50ada6dd136e2b161453943
Mohammad Baluchi <h.baloochi@gmail.com>
2022-05-23 16:33:39 +0200

    added CD to deploy to Heroku

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages
 from setuptools import setup
-
+#mysetup
 with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
